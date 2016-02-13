5d4a7daf40c2a970bfba3c9767ce7ed090db8205
leoviathan <euleoo@gmail.com>
2016-02-13 08:55:25 -0200

    Basic navigation prototype done;

---
+++ Assets/TowerDefense/Scripts/MoveToDestination.cs
@@ -0,0 +1,22 @@
+﻿// ------------------------------------------------------------------------------------------------------------
+// 							 MoveToDestination.cs
+// 				 Copyright (c) 2016 Leonardo Lopes
+//  	Authors: Leonardo M. Lopes <euleoo@gmail.com> - http://about.me/leonardo_lopes
+// ------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class MoveToDestination : MonoBehaviour {
+	private Transform destination;
+	private NavMeshAgent agent;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+
+		destination = GameObject.FindWithTag("Finish").transform;
+
+		agent.destination = destination.position;
+	}
+}
